198cc413c3123d8483af9e7eac69c29ddd0bc748
Александр <aBekar145@gmail.com>
2020-08-20 21:59:29 +0300

    started learning objects

---
+++ Practice JS Minin/05 array .js
@@ -0,0 +1,113 @@
+// Массивы 03:03
+
+const cars = ["Mazda", "Ford", "Bmw", "Mersedes"];
+const fib = [1, 1, 2, 3, 5, "21", true];
+const people = [{
+    name: "Vladilen",
+    budget: 4200
+  },
+  {
+    name: "Elena",
+    budget: 3500
+  },
+  {
+    name: "Victoria",
+    budget: 1700
+  },
+];
+
+// Function
+function addItemToEnd() {}
+
+// Method
+// cars.push("Porshe");
+// cars.unshift("Kia");
+// const firstCar = cars.shift();
+// const lastCar = cars.pop();
+
+// console.log(firstCar);
+// console.log(cars);
+
+// console.log(cars.reverse());
+
+// Task 1
+
+// const text = "Hello, We learn JavaScript";
+
+// const reverseText = text.split(',').reverse().join(' ');
+// console.log(reverseText);
+
+// const index = people.findIndex(function (person) {
+//   return person.budget === 1700;
+// });
+// const person = people.find(function (person) {
+//   return person.budget === 1700;
+// });
+
+// let findedPerson;
+
+// for (const person of people) {
+
+//   if (person.budget === 3500) {
+//     findedPerson = person;
+//   }
+// }
+
+// const person = people.find(person => person.budget === 3500)
+
+
+// console.log(person);
+
+// console.log(cars.includes('Mazda'));
+
+// const upperCaseCars = cars.map(car => {
+//     return car.toUpperCase();
+// })
+
+// const pow2 = num => num ** 2;
+// const sqrt = num => Math.sqrt(num);
+
+// const pow2Fib = fib.map(pow2);
+
+// const filteredNumbers = pow2Fib.filter(num => num > 20);
+// console.log(pow2Fib);
+// console.log(filteredNumbers);
+
+
+
+
+
+
+
+
+
+// 3:38
+
+
+// Easy Code School
+
+// const numArr = [2, 32, 1234, 54, 323];
+
+// let value;
+
+// value = numArr.length;
+// // numArr.length = 100;
+
+// value = Array.isArray(numArr);
+// value = numArr[2];
+// numArr[2] = 12;
+// value = numArr.indexOf(54);
+
+// value = numArr.push(100);
+// value = numArr.pop();
+// value = numArr.unshift(111);
+// value = numArr.shift(111);
+// value = numArr.slice(0, 2);
+// value = numArr.splice(1, 2, 'one', 'two');
+// value = numArr.reverse();
+// value = numArr.concat(1, 2, 3);
+// value = numArr.join();
+// value = 'Hello world'.split();
+
+// console.log(value, numArr);
+// console.log(value);
--- Practice JS Minin/app.js
+++ Practice JS Minin/app.js
@@ -1,95 +1,54 @@
-// Массивы 03:03
+// Objects
 
-const cars = ["Mazda", "Ford", "Bmw", "Mersedes"];
-const fib = [1, 1, 2, 3, 5, "21", true];
-const people = [
-  { name: "Vladilen", budget: 4200 },
-  { name: "Elena", budget: 3500 },
-  { name: "Victoria", budget: 1700 },
-];
+// Easy Code 
 
-// Function
-function addItemToEnd() {}
-
-// Method
-// cars.push("Porshe");
-// cars.unshift("Kia");
-// const firstCar = cars.shift();
-// const lastCar = cars.pop();
-
-// console.log(firstCar);
-// console.log(cars);
-
-// console.log(cars.reverse());
-
-// Task 1
-
-// const text = "Hello, We learn JavaScript";
-
-// const reverseText = text.split(',').reverse().join(' ');
-// console.log(reverseText);
-
-// const index = people.findIndex(function (person) {
-//   return person.budget === 1700;
-// });
-// const person = people.find(function (person) {
-//   return person.budget === 1700;
-// });
-
-// let findedPerson;
-
-// for (const person of people) {
-
-//   if (person.budget === 3500) {
-//     findedPerson = person;
-//   }
-// }
-
-// const person = people.find(person => person.budget === 3500)
-
-
-// console.log(person);
-
-// console.log(cars.includes('Mazda'));
-
-// const upperCaseCars = cars.map(car => {
-//     return car.toUpperCase();
-// })
-
-// const pow2 = num => num ** 2;
-// const sqrt = num => Math.sqrt(num);
-
-// const pow2Fib = fib.map(pow2);
-
-// const filteredNumbers = pow2Fib.filter(num => num > 20);
-// console.log(pow2Fib);
-// console.log(filteredNumbers);
-
-// 3:38
-
-// Easy Code School
-
-const numArr = [2, 32, 1234, 54, 323];
+const user = {
+  firstName: 'Denis',
+  age: 30,
+  isAdmin: true,
+  email: 'test@test.com',
+  'user-address': {
+    city: 'Kharkiv'
+  },
+  skills: ['html', 'css', 'js']
+};
 
 let value;
+let prop = 'skills';
 
-value = numArr.length;
-// numArr.length = 100;
 
-value = Array.isArray(numArr);
-value = numArr[2];
-numArr[2] = 12;
-value = numArr.indexOf(54);
+value = user.firstName;
 
-value = numArr.push(100);
-value = numArr.pop();
-value = numArr.unshift(111);
-value = numArr.shift(111);
-value = numArr.slice(0, 2);
-value = numArr.splice(1, 2, 'one', 'two');
-value = numArr.reverse();
-value = numArr.concat(1, 2, 3);
-value = numArr.join();
-value = 'Hello world'.split();
+value = user['isAdmin'];
 
-console.log(value, numArr);
+value = user['user-address'];
+
+value = user['user-address'];
+value = user['skills'];
+value = user[prop][1];
+
+
+user.firstName = 'Den';
+
+value = user.firstName;
+
+user.info = 'Some info';
+
+value = user.info;
+
+user['user-address'].city = 'Kiev';
+user['user-address'].country = 'Ukraine';
+
+console.log(user.plan);
+user.plan = {};
+
+user.plan.basic = 'basic';
+
+console.log(value);
+console.log(user);
+
+
+
+
+
+// 3:45 (Minin practice)
