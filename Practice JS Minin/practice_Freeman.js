e0e98b65256db6c9bf6b03bd0728ba18b04d6ab7
aBekar145 <alexdream81@gmail.com>
2020-08-21 18:44:51 +0300

    added fiat object

--- Practice JS Minin/practice_Freeman.js
+++ Practice JS Minin/practice_Freeman.js
@@ -14,4 +14,14 @@ let miles = fiat.mileage;
 fiat.mileage = 10000;
 fiat.needsWashing = true;
 
+fiat.maxSpeed = 100;
+
+// if (fiat.year < 1965) {
+//     classic = true;
+// };
+
+// for (let i = 0; i < fiat.passengers; i++) {
+//     addPersonToCar();
+// };
+
 console.log(fiat);
